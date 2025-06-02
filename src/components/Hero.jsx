8c5668a1a0bb5a61bb7748776b0f03cca876d0cf
Eugene <eugenelevkovych@yahoo.com>
2025-06-02 23:59:41 +0300

    add input

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -2,7 +2,7 @@ export default function Hero() {
 
   return (
 <div className="container flex items-center py-5 mx-auto px-4">
-    <div className="flex justify-center items-center size-12 bg-blue1 mr-4">
+    <div className="flex justify-center items-center size-12 rounded-lg bg-blue1 mr-4">
       <svg className="w-[1.6rem] h-[1.1rem] stroke-main-blue fill-transparent">
         <use href="./sprite.svg#icon-burger1"></use>
       </svg>
@@ -11,6 +11,15 @@ export default function Hero() {
       <svg className="w-[8.6rem] h-7 fill-main-blue">
         <use href="./sprite.svg#icon-MegaMart"></use>
       </svg>
-    </a>   
+    </a>
+    <div className="relative max-w-[31.7rem] w-full"> 
+        <svg className="absolute size-[18px] bottom-4 left-4 stroke-main-blue fill-transparent">
+         <use href="./sprite.svg#icon-search"></use>
+        </svg>
+      <input className="h-12 w-full bg-blue1 pl-11" type="text" placeholder="Search essentials, groceries and more..." />
+        <svg className="absolute size-[18px] right-4 bottom-4 stroke-main-blue fill-main-blue">
+         <use href="./sprite.svg#icon-burger2"></use>
+        </svg>
+    </div>  
 </div>
 )}
