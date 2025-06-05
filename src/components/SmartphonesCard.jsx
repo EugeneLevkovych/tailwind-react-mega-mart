8f1cfa55e29d0bf7b69185c07047337ea4eae906
Eugene <eugenelevkovych@yahoo.com>
2025-06-05 22:30:30 +0300

    add ovfl to card

--- src/components/SmartphonesCard.jsx
+++ src/components/SmartphonesCard.jsx
@@ -2,19 +2,17 @@ export default function SmartphonesCard({smartphoneObj}) {
     const price = Number(smartphoneObj.price.replace('$', ''));
     const prevPrice = Number(smartphoneObj.prevPrice.replace('$', ''));
   return (
-    <li className="max-w-[14.2rem] w-full h-[18.5rem] border-[1px] border-solid border-gray3 rounded-2xl">
+    <li className="max-w-[14.3rem] w-full h-[18.5rem] border-[1px] border-solid border-gray3 rounded-2xl overflow-hidden">
         <div className="flex justify-center h-[11.8rem] bg-gray1">
           <img className="h-[11.8rem] w-[8.8rem] object-scale-down" src={smartphoneObj.photoName} />
         </div>
-        <div className="p-3">
-        
-           <p>{smartphoneObj.name}</p>
-           <div className="flex gap-2.5 border-b-1 border-gray3">
+        <div className="p-3 text-gray4">
+           <p className="font-semibold leading-[1.2] mb-2.5">{smartphoneObj.name}</p>
+           <div className="flex gap-2.5 border-b-1 border-gray3 mb-2.5">
            <p>{smartphoneObj.price}</p>
-        
            <p>{smartphoneObj.prevPrice}</p>
            </div>
-        <p>Save - $ {prevPrice - price}</p>
+        <p className="text-green1">Save - $ {prevPrice - price}</p>
         </div>
     </li>
   )}
