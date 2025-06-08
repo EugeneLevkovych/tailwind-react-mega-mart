8f8acddeff5eb485ae36d761e3e2f3f51113886d
Eugene <eugenelevkovych@yahoo.com>
2025-06-08 22:09:27 +0300

    add more elipses

--- src/components/Advertisement.jsx
+++ src/components/Advertisement.jsx
@@ -5,25 +5,14 @@ export default function Advertisement() {
 
   return (
     <div className="container relative flex justify-between border-0 rounded-2xl bg-blue2 mb-[7.5rem] mx-auto px-4 overflow-hidden">
-         <div className="absolute -top-75 -right-2 size-116.5 border-2 rounded-full"
-        style={{
-         backgroundImage: `linear-gradient(to bottom, rgba(33, 40, 68, 0.9), rgba(33, 40, 68, 0.9)), url(${ellipse})`,
-         backgroundSize: '426px',
-         backgroundPosition: 'center',
-         backgroundRepeat: 'no-repeat',
-         borderColor: 'var(--color-decor-blue)',
-          }} />
-
-             <div className="absolute -bottom-20 right-60 size-37 border-2 rounded-full"
-        style={{
-         backgroundImage: `linear-gradient(to bottom, rgba(33, 40, 68, 0.9), rgba(33, 40, 68, 0.9)), url(${ellipse})`,
-         backgroundSize: '118px',
-         backgroundPosition: 'center',
-         backgroundRepeat: 'no-repeat',
-         borderColor: 'var(--color-decor-blue)',
-          }} />
-         
-
+        <div className="absolute -top-75 -right-2 flex justify-center items-center size-116.5 border-2 rounded-full" style={{borderColor: `var(--color-border-decor-blue)`}}>
+            <div className="size-105.5 rounded-full" style={{backgroundColor: `var(--color-decor-blue)`}} />
+        </div>
+       
+        <div className="absolute -bottom-28 right-60 flex justify-center items-center size-46 border-2 rounded-full" style={{borderColor: `var(--color-border-decor-blue)`}}>
+            <div className="size-38.5 rounded-full" style={{backgroundColor: `var(--color-decor-blue)`}} />
+        </div>
+              
         <div className="lg:ml-10 xl:ml-[5.62rem] mt-10 space-y-5 z-10">
           <p className="font-semibold text-[1.2rem] lg:text-[1.9rem] leading-none text-white1">Best Deal Online on smart watches</p>
           <p className="font-bold text-[2rem] lg:text-[3.9rem] leading-none text-white1">SMART WEARABLE.</p>
---
+++ src/data/dataAdvertisement.js
@@ -0,0 +1,36 @@
+import watch1 from "../assets/smart-wearable.png";
+import watch2 from "../assets/mi-band-pro-7.webp";
+import watch3 from "../assets/smartband-mi-band-9";
+
+export const ADVERTISEMENT = [
+     {
+            name: "Smart Wearable",
+            id: 1,
+            photoBrandsName: electronicsBrandsImg1,
+            photoName: watch1,
+            discont: "UP to 80% OFF",
+            backColor: "gray6",
+            decorColor: "gray5",
+            textColor: "text-white",
+        },
+            {
+            name: "Mi Band Pro 7",
+            id: 2,
+            photoBrandsName: electronicsBrandsImg2,
+            photoName: watch2,
+            discont: "UP to 80% OFF",
+            backColor: "yellow1",
+            decorColor: "yellow2",
+            textColor: "text-dark",
+        },
+             {
+            name: "Smartband Mi Band 9",
+            id: 3,
+            photoBrandsName: electronicsBrandsImg3,
+            photoName: watch3,
+            discont: "UP to 80% OFF",
+            backColor: "peach2",
+            decorColor: "peach1",
+            textColor: "text-dark",
+        },
+]
