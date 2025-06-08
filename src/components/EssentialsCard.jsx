779456ad204e53b988d1ca58abbe670bc7e6a4ce
Eugene <eugenelevkovych@yahoo.com>
2025-06-08 11:49:00 +0300

    add shadows

--- src/components/EssentialsCard.jsx
+++ src/components/EssentialsCard.jsx
@@ -2,7 +2,7 @@ export default function EssentialCard({essentialsObj}) {
 
   return (
     <li className="w-46.5 h-auto border-none">
-        <div className="size-46.5 flex justify-center items-center bg-gray1 rounded-2xl mb-5">
+        <div className="size-46.5 flex justify-center items-center bg-gray1 rounded-2xl hover:border hover:border-blue4 hover:shadow-lg mb-5">
           <img src={essentialsObj.photoName} />
         </div>
         <p className="font-semibold leading-[1.25] text-center text-gray2 mb-1">{essentialsObj.name}</p>
--- src/components/SmartphonesCard.jsx
+++ src/components/SmartphonesCard.jsx
@@ -1,13 +1,14 @@
 export default function SmartphonesCard({smartphoneObj}) {
     const price = Number(smartphoneObj.price.replace('$', ''));
     const prevPrice = Number(smartphoneObj.prevPrice.replace('$', ''));
+
   return (
-    <li className="relative w-[14.3rem] h-[18.5rem] border-[1px] border-solid border-gray3 rounded-2xl overflow-hidden">
+    <li className="relative w-[14.3rem] h-[18.5rem] border-[1px] border-solid border-gray3 rounded-2xl hover:border hover:border-solid hover:border-blue4 hover:shadow-xl overflow-hidden">
         <div className="absolute top-0 right-0 flex items-center w-[3.18rem] h-[3.31rem] font-semibold text-sm text-white1 uppercase bg-main-blue rounded-bl-2xl pl-2.5">56% Off</div>
         <div className="flex justify-center h-[11.8rem] bg-gray1">
           <img className="h-[11.8rem] w-[8.8rem] object-scale-down" src={smartphoneObj.photoName} />
         </div>
-        <div className="p-1 xl:p-3 text-gray4">
+        <div className="p-3 text-gray4">
           <p className="font-medium sm:font-semibold text-sm md:text-base leading-[1.2] mb-1 xl:mb-2.5">{smartphoneObj.name}</p>
           <div className="flex gap-2.5 border-b-1 border-gray3 mb-1 xl:mb-2.5">
             <p>{smartphoneObj.price}</p>
