2dd4133885f4a6694cdd13dde97ed0683e78435c
Eugene <eugenelevkovych@yahoo.com>
2025-06-09 21:19:16 +0300

    add more cards

--- src/data/dataElectronicsBrands.js
+++ src/data/dataElectronicsBrands.js
@@ -1,9 +1,14 @@
 import electronicsBrandsImg1 from "../assets/apple.png";
 import electronicsBrandsImg2 from "../assets/realme.png";
 import electronicsBrandsImg3 from "../assets/mi.png";
+import electronicsBrandsImg4 from "../assets/nokia.png";
+import electronicsBrandsImg5 from "../assets/oppo.png";
+
 import brands1 from "../assets/brands1.png";
 import brands2 from "../assets/brands2.png";
 import brands3 from "../assets/brands3.png";
+import brands4 from "../assets/brands4.png";
+import brands5 from "../assets/brands5.png";
 
 export const ELECTRONICS_BRANDS = [
        {
@@ -14,7 +19,7 @@ export const ELECTRONICS_BRANDS = [
         discont: "UP to 80% OFF",
         backColor: "gray6",
         decorColor: "gray5",
-        textColor: "text-white",
+        textColor: "white",
     },
         {
         name: "Realme",
@@ -24,7 +29,7 @@ export const ELECTRONICS_BRANDS = [
         discont: "UP to 80% OFF",
         backColor: "yellow1",
         decorColor: "yellow2",
-        textColor: "text-dark",
+        textColor: "gray4",
     },
          {
         name: "Realme",
@@ -34,6 +39,26 @@ export const ELECTRONICS_BRANDS = [
         discont: "UP to 80% OFF",
         backColor: "peach2",
         decorColor: "peach1",
-        textColor: "text-dark",
+        textColor: "gray4",
+    },
+             {
+        name: "Nokia",
+        id: 4,
+        photoBrandsName: electronicsBrandsImg4,
+        photoName: brands4,
+        discont: "UP to 80% OFF",
+        backColor: "green1",
+        decorColor: "green2",
+        textColor: "gray4",
+    },
+             {
+        name: "Oppo",
+        id: 5,
+        photoBrandsName: electronicsBrandsImg5,
+        photoName: brands5,
+        discont: "UP to 80% OFF",
+        backColor: "red1",
+        decorColor: "red2",
+        textColor: "white",
     },
 ]
